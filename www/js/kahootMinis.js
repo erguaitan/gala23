252533f87286ef5fea426b012c1d3c9e9d513ca9
erguaitan <albertodiazpacheco09@gmail.com>
2023-12-20 07:03:50 +0100

    ranking kahoot

--- www/js/kahootMinis.js
+++ www/js/kahootMinis.js
@@ -20,6 +20,11 @@ let listUser = [];
 let resetNum;
 let listaVot = 0;
 
+let rankPts = [0, 0, 0];
+let rankUser = ["...", "...", "..."];
+let ptsUser = []
+let inicioTiempoPreg;
+
 
 fetch("../minis/user.json")
 .then(response => response.json())
@@ -47,6 +52,50 @@ function eliminarChildsDeClase(className) {
     }
 }
 
+function calculatePuntuaje(user, tiempoFin){
+    let diferenciaEnMilisegundos = tiempoFin - inicioTiempoPreg;
+    let diferenciaEnSegundos = diferenciaEnMilisegundos / 1000;
+    let pts;
+
+    if (diferenciaEnSegundos === 0) {
+        pts = 1000; 
+    } else if (diferenciaEnSegundos >= 20) {
+        pts = 0;
+    } else {
+        let puntaje = 1000 - Math.floor(diferenciaEnSegundos * 50);
+        pts = Math.max(0, puntaje); 
+    }
+
+    ptsUser[listUser.indexOf(user)] = ptsUser[listUser.indexOf(user)] + pts;
+    
+    actualizarRanking(user, ptsUser[listUser.indexOf(user)])
+}
+
+function actualizarRanking(user, pts) {
+    var index = rankUser.indexOf(user);
+
+    if (index !== -1) {
+    rankPts[index] = pts;
+    } else {
+    rankUser.push(user);
+    rankPts.push(pts);
+    }
+
+    var sortedRanking = rankPts.map(function (value, index) {
+    return { user: rankUser[index], pts: value };
+    }).sort(function (a, b) {
+    return b.pts - a.pts;
+    });
+
+    rankUser = sortedRanking.slice(0, 3).map(function (element) {
+    return element.user;
+    });
+
+    rankPts = sortedRanking.slice(0, 3).map(function (element) {
+    return element.pts;
+    });
+}
+
 
 //RECIBE SOCKET ACCIÓN_ACERTAR
 socket.on("ACCIÓN_KAHOOT", (act)=>{
@@ -61,6 +110,8 @@ socket.on("ACCIÓN_KAHOOT", (act)=>{
         listaVot = 0;
         document.getElementById("kahootPreg").innerHTML = resetNum;
         eliminarChildsDeClase("imgUsers");
+
+        inicioTiempoPreg = new Date().getTime();
 
         if (pregunta.style.display == "none"){
             noneScreens();
@@ -118,6 +169,10 @@ socket.on("ACCIÓN_KAHOOT", (act)=>{
             socket.emit("RESOL", ["preg"+numPreg, data.pregKahoot[numPreg-1], data.solKahoot[numPreg-1]]);
         })
         .catch(error => console.error("Error al cargar el archivo JSON: " + error));
+
+        document.getElementById("pos1").innerHTML = rankUser[0] + "<p>"+ rankPts[0] +"pts.</p>"
+        document.getElementById("pos2").innerHTML = rankUser[1] + "<p>"+ rankPts[1] +"pts.</p>"
+        document.getElementById("pos3").innerHTML = rankUser[2] + "<p>"+ rankPts[2] +"pts.</p>"
     }
 });
 //RECIBE SOCKET CAMBIO
@@ -154,6 +209,9 @@ socket.on("VOTACION", (dataAcertar)=>{
                 document.querySelector(".imgUsers").appendChild(userCorrect);
             })
             .catch(error => console.error("Error al cargar el archivo JSON: " + error));  
+
+            let tiempoEnviado = new Date().getTime();
+            calculatePuntuaje(userName, tiempoEnviado);
         }
     })
     .catch(error => console.error("Error al cargar el archivo JSON: " + error));
@@ -163,6 +221,7 @@ socket.on("VOTACION", (dataAcertar)=>{
 socket.on("NEWUSER", (dataUser)=>{
     if (listUser.indexOf(dataUser) == -1){
         listUser.push(dataUser);
+        ptsUser.push(0);
     }
     if (document.getElementById("kahootTit").innerHTML[2] == "/"){
         document.getElementById("kahootTit").innerHTML = listUser.length + document.getElementById("kahootTit").innerHTML.substring(1);
